4334d8cedded9aae85209eb7fac36a09d03f5f76
Polina Shlepakova <polinashlepakova@gmail.com>
2019-02-15 00:50:11 +0200

    1st version of anagrams on JS
    - string sorting
    - find anagrams in array
        using string sorting and map
    - display on the screen
    
    To add: get dictionary from file.
    
    Test:
    node Anagrams/JS/anagrams.js
    Expected result:
    tovar - avtor - tavro
    aroza - azora
    
    Signed-off-by: Polina Shlepakova <polinashlepakova@gmail.com>

---
+++ Anagrams/JS/anagrams.js
@@ -0,0 +1,28 @@
+function sort_string(str) {
+    return str.split("").sort().join("");
+}
+
+function find_anagrams(arr) {
+    var map = [];
+    for (var i = 0; i < arr.length; i++) {
+        var sorted = sort_string(arr[i]);
+        if (map[sorted] == null) {
+            // not in map
+            map[sorted] = [arr[i]];
+        } else {
+            map[sorted].push(arr[i]);
+        }
+    }
+    for (var key in map) {
+        if (map[key].length > 1) {
+            var res = "";
+            for (var i = 0; i < map[key].length; i++) {
+                res += map[key][i] + (i == map[key].length - 1 ? "" : " - ");
+            }
+            console.log(res);
+        }
+    }
+}
+
+var words = ["tovar", "aroza", "dfd", "avtor", "azora", "tavro"];
+find_anagrams(words);
